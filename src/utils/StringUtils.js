43c9a5705bea33e3e13b6a8c341be7c414e4af53
Taylor Sweetman <taylor.sweetman@gmail.com>
2020-12-26 11:28:31 -0500

    wrote urlifyStr()

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -1,17 +1,30 @@
 export const beautifyStr = (inputStr) => {
-	console.log('input', inputStr);
 	var parts = inputStr.split('_');
 	var outputStr = '';
 
 	for (var idx in parts) {
-		console.log('part', parts[idx]);
 		const firstChar = parts[idx].charAt(0).toUpperCase();
 		const newWord = firstChar + parts[idx].slice(1);
-		console.log('new word', newWord);
 		if (outputStr.length === 0) {
 			outputStr += newWord;
 		} else {
 			outputStr += ' ' + newWord;
+		}
+	}
+
+	return outputStr;
+};
+
+export const urlifyStr = (inputStr) => {
+	var parts = inputStr.split(' ');
+	var outputStr = '';
+
+	for (var idx in parts) {
+		const newWord = parts[idx].toLowerCase();
+		if (outputStr.length === 0) {
+			outputStr += newWord;
+		} else {
+			outputStr += '_' + newWord;
 		}
 	}
 
